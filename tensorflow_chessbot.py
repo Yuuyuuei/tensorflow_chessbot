c75cd3824947117fb342dab3b6af375e7d03fd7a
Eugene Tan <eugene@eugenetan.co.uk>
2020-12-17 02:00:25 +0000

    Added a json option that returns a json string

--- tensorflow_chessbot.py
+++ tensorflow_chessbot.py
@@ -8,44 +8,48 @@
 #
 #   $ ./tensorflow_chessbot.py -h
 #   usage: tensorflow_chessbot.py [-h] [--url URL] [--filepath FILEPATH]
-# 
+#
 #    Predict a chessboard FEN from supplied local image link or URL
-# 
+#
 #    optional arguments:
 #      -h, --help           show this help message and exit
 #      --url URL            URL of image (ex. http://imgur.com/u4zF5Hj.png)
 #     --filepath FILEPATH  filepath to image (ex. u4zF5Hj.png)
-# 
-# This file is used by chessbot.py, a Reddit bot that listens on /r/chess for 
-# posts with an image in it (perhaps checking also for a statement 
+#
+# This file is used by chessbot.py, a Reddit bot that listens on /r/chess for
+# posts with an image in it (perhaps checking also for a statement
 # "white/black to play" and an image link)
-# 
+#
 # It then takes the image, uses some CV to find a chessboard on it, splits it up
 # into a set of images of squares. These are the inputs to the tensorflow CNN
 # which will return probability of which piece is on it (or empty)
-# 
+#
 # Dataset will include chessboard squares from chess.com, lichess
 # Different styles of each, all the pieces
-# 
+#
 # Generate synthetic data via added noise:
 #  * change in coloration
 #  * highlighting
 #  * occlusion from lines etc.
-# 
+#
 # Take most probable set from TF response, use that to generate a FEN of the
 # board, and bot comments on thread with FEN and link to lichess analysis.
-# 
-# A lot of tensorflow code here is heavily adopted from the 
+#
+# A lot of tensorflow code here is heavily adopted from the
 # [tensorflow tutorials](https://www.tensorflow.org/versions/0.6.0/tutorials/pdes/index.html)
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # Ignore Tensorflow INFO debug messages
-import tensorflow as tf
-import numpy as np
 
-from helper_functions import shortenFEN, unflipFEN
-import helper_image_loading
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # Ignore Tensorflow INFO debug messages
+import json
+
+import numpy as np
+import tensorflow as tf
+
 import chessboard_finder
+import helper_image_loading
+from helper_functions import shortenFEN, unflipFEN
+
 
 def load_graph(frozen_graph_filepath):
     # Load and parse the protobuf file to retrieve the unserialized graph_def.
@@ -63,7 +67,8 @@ class ChessboardPredictor(object):
   """ChessboardPredictor using saved model"""
   def __init__(self, frozen_graph_path='saved_models/frozen_graph.pb'):
     # Restore model using a frozen graph.
-    print("\t Loading model '%s'" % frozen_graph_path)
+    if not args.json:
+      print("\t Loading model '%s'" % frozen_graph_path)
     graph = load_graph(frozen_graph_path)
     self.sess = tf.compat.v1.Session(graph=graph)
 
@@ -72,22 +77,23 @@ class ChessboardPredictor(object):
     self.keep_prob = graph.get_tensor_by_name('tcb/KeepProb:0')
     self.prediction = graph.get_tensor_by_name('tcb/prediction:0')
     self.probabilities = graph.get_tensor_by_name('tcb/probabilities:0')
-    print("\t Model restored.")
+    if not args.json:
+      print("\t Model restored.")
 
   def getPrediction(self, tiles):
     """Run trained neural network on tiles generated from image"""
     if tiles is None or len(tiles) == 0:
       print("Couldn't parse chessboard")
       return None, 0.0
-    
+
     # Reshape into Nx1024 rows of input data, format used by neural network
     validation_set = np.swapaxes(np.reshape(tiles, [32*32, 64]),0,1)
 
     # Run neural network on data
     guess_prob, guessed = self.sess.run(
-      [self.probabilities, self.prediction], 
+      [self.probabilities, self.prediction],
       feed_dict={self.x: validation_set, self.keep_prob: 1.0})
-    
+
     # Prediction bounds
     a = np.array(list(map(lambda x: x[0][x[1]], zip(guess_prob, guessed))))
     tile_certainties = a.reshape([8,8])[::-1,:]
@@ -104,7 +110,7 @@ class ChessboardPredictor(object):
     """Try and return a FEN prediction and certainty for URL, return Nones otherwise"""
     img, url = helper_image_loading.loadImageFromURL(url, max_size_bytes=2000000)
     result = [None, None, None]
-    
+
     # Exit on failure to load image
     if img is None:
       print('Couldn\'t load URL: "%s"' % url)
@@ -125,10 +131,10 @@ class ChessboardPredictor(object):
     if tiles is None:
       print('Couldn\'t find chessboard in image')
       return result
-    
+
     # Make prediction on input tiles
     fen, tile_certainties = self.getPrediction(tiles)
-    
+
     # Use the worst case certainty as our final uncertainty score
     certainty = tile_certainties.min()
 
@@ -140,7 +146,8 @@ class ChessboardPredictor(object):
     return result
 
   def close(self):
-    print("Closing session.")
+    if not args.json:
+      print("Closing session.")
     self.sess.close()
 
 ###########################################################
@@ -158,7 +165,7 @@ def main(args):
   # Exit on failure to load image
   if img is None:
     raise Exception('Couldn\'t load URL: "%s"' % args.url)
-    
+
   # Resize image if too large
   # img = helper_image_loading.resizeAsNeeded(img)
 
@@ -172,13 +179,16 @@ def main(args):
   # Create Visualizer url link
   if args.url:
     viz_link = helper_image_loading.getVisualizeLink(corners, args.url)
-    print('---\nVisualize tiles link:\n %s\n---' % viz_link)
+    if not args.json:
+      print('---\nVisualize tiles link:\n %s\n---' % viz_link)
 
   if args.url:
-    print("\n--- Prediction on url %s ---" % args.url)
+    if not args.json:
+      print("\n--- Prediction on url %s ---" % args.url)
   else:
-    print("\n--- Prediction on file %s ---" % args.filepath)
-  
+    if not args.json:
+      print("\n--- Prediction on file %s ---" % args.filepath)
+
   # Initialize predictor, takes a while, but only needed once
   predictor = ChessboardPredictor()
   fen, tile_certainties = predictor.getPrediction(tiles)
@@ -189,14 +199,21 @@ def main(args):
   # Use the worst case certainty as our final uncertainty score
   certainty = tile_certainties.min()
 
-  print('Per-tile certainty:')
-  print(tile_certainties)
-  print("Certainty range [%g - %g], Avg: %g" % (
-    tile_certainties.min(), tile_certainties.max(), tile_certainties.mean()))
+  if not args.json:
+    print('Per-tile certainty:')
+    print(tile_certainties)
+    print("Certainty range [%g - %g], Avg: %g" % (
+      tile_certainties.min(), tile_certainties.max(), tile_certainties.mean()))
 
   active = args.active
-  print("---\nPredicted FEN:\n%s %s - - 0 1" % (short_fen, active))
-  print("Final Certainty: %.1f%%" % (certainty*100))
+  if not args.json:
+    print("---\nPredicted FEN:\n%s %s - - 0 1" % (short_fen, active))
+    print("Final Certainty: %.1f%%" % (certainty*100))
+  else:
+    print(json.dumps({
+      "fen": "%s %s - - 0 1" % (short_fen, active),
+      "certainty": "%.1f%%" % (certainty*100)
+    }))
 
 if __name__ == '__main__':
   np.set_printoptions(suppress=True, precision=3)
@@ -206,7 +223,6 @@ if __name__ == '__main__':
   parser.add_argument('--filepath', help='filepath to image (ex. u4zF5Hj.png)')
   parser.add_argument('--unflip', default=False, action='store_true', help='revert the image of a flipped chessboard')
   parser.add_argument('--active', default='w')
+  parser.add_argument('--json', default=False, action='store_true', help='returns the data in json format')
   args = parser.parse_args()
   main(args)
-
-  
